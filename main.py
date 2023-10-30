bd14e8e5129f39bb71bf51c3451d7f797ca73aa6
Justin Huynh <justinhuynh639@gmail.com>
2023-10-30 12:57:02 -0700

    defined neighbors

--- main.py
+++ main.py
@@ -40,6 +40,42 @@ def find_path(maze, stdscr):
     q = queue.Queue()
     q.put((start_pos, [start_pos]))
 
+    visited = set()
+
+    while not q.empty():
+        current_pos, path = q.get()
+        row, col = current_pos
+
+        if maze[row][col] == end:
+            return path
+
+        neighbors = find_neighbors(maze, row, col)
+        for neighbor in neighbors:
+            if neighbor in visited:
+                continue
+
+            r, c = neighbor
+            if maze[r][c] == "#":
+                continue
+
+            new_path = path + [neighbor]
+            q.put((neighbor, new_path))
+            visited.add(neighbor)
+
+def find_neighbors(maze, row, col):
+    neighbors = []
+
+    if row > 0: # UP
+        neighbors.append((row - 1, col))
+    if row + 1 < len(maze):
+        neighbors.append((row + 1, col))
+    if col > 0: # LEFT
+        neighbors.append((row, col -1))
+    if col + 1 < len(maze[0]):
+        neighbors.append((row, col + 1))
+
+    return neighbors
+
 def main(stdscr):
     # change text and background color
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)

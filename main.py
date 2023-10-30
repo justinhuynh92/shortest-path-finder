39feca2fbf8b33d5b6df495b865a0f78be04bc7d
Justin Huynh <justinhuynh639@gmail.com>
2023-10-30 12:48:38 -0700

    find starting pos

--- main.py
+++ main.py
@@ -15,12 +15,38 @@ maze = [
     ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
 ]
 
+def print_maze(maze, stdscr, path=[]):
+    BLUE = curses.color_pair(1)
+    RED = curses.color_pair(2)
+
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            # determine where we want to add the string
+            stdscr.addstr(i, j*2, value, BLUE)
+
+def find_start(maze, start):
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            if value == start:
+                return i, j
+
+    return None
+
+def find_path(maze, stdscr):
+    start = "O"
+    end = "X"
+    start_pos = find_start(maze, start)
+
+    q = queue.Queue()
+    q.put((start_pos, [start_pos]))
+
 def main(stdscr):
+    # change text and background color
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
-    blue_and_black = curses.color_pair(1)
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
 
     stdscr.clear()
-    stdscr.addstr(5, 5, "hello world!", blue_and_black)
+    print_maze(maze, stdscr)
     stdscr.refresh()
     stdscr.getch()
 
